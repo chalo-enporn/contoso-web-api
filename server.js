11f02481b4551e10c791afa5efb7fe827ff0b3d0
Sittisak Apai <sittiapa@ais.co.th>
2024-11-06 08:47:48 +0000

    commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const host = '0.0.0.0';
 const app = express();
 
 app.get('/',(req,res) => {
-    res.send(`Hello World - 2.0.0`);
+    res.send(`Hello World - 3.0.0`);
 });
 app.listen(port,host,() => {
     console.log(`Running On http://${host}:${port}`);
